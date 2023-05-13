2cf21203368f9da1c89cbda4c7d11a9187fc7773
kharanzhevskii <kharanzhevskii.ne@phystech.edu>
2023-05-13 22:59:49 +0300

    created an sfml window, which does nothing but open and close correctly

---
+++ board.hpp
@@ -0,0 +1,21 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <array>
+
+const int BOARD_SIZE = 10;
+
+class Board : public sf::Drawable {
+public:
+    Board(sf::Color col1 = sf::Color::White, sf::Color col2 = sf::Color::Black);    
+    // loads the sprites of board' rectangles
+    void load(sf::Color col1 = sf::Color::White, sf::Color col2 = sf::Color::Black);
+private:
+    std::array<sf::RectangleShape, 100> m_boardSquares;    
+    // draw class on SFML Window, const for target
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+};
+
+#endif
